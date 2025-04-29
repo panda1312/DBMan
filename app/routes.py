95118e44144925fef89bf2cf4413c6f24d5247f0
panda1312 <33173122+panda1312@users.noreply.github.com>
2025-04-29 09:48:04 +0900

    Update routes.py
    added query

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,30 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import SQLAlchemyError
 
 main = Blueprint('main', __name__)
 db = SQLAlchemy()
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    result = db.session.execute('SELECT name FROM sqlite_master WHERE type="table";')
-    tables = [row[0] for row in result]
-    return render_template('index.html', tables=tables)
+    tables = []
+    query_result = None
+    error = None
+
+    if request.method == 'POST':
+        sql_query = request.form.get('query')
+        try:
+            result = db.session.execute(sql_query)
+            query_result = result.fetchall()
+            db.session.commit()
+        except SQLAlchemyError as e:
+            error = str(e.__dict__.get('orig'))
+            db.session.rollback()
+    
+    try:
+        result = db.session.execute('SELECT name FROM sqlite_master WHERE type="table";')
+        tables = [row[0] for row in result]
+    except Exception as e:
+        error = str(e)
+
+    return render_template('index.html', tables=tables, query_result=query_result, error=error)
