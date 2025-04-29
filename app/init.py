23d69e9bf5774f8a9e798e19f253e170ed3d3314
panda1312 <33173122+panda1312@users.noreply.github.com>
2025-04-29 09:50:07 +0900

    Update and rename __init__.py to init.py

--- app/init.py
+++ app/init.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from .routes import main
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'  # Temporary
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    from .routes import main
+    db.init_app(app)
     app.register_blueprint(main)
 
     return app
